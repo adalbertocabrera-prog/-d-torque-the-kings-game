b6e995209ed9af95fdecd7622c980ef7cc03da53
adalbertocabrera-prog <adalberto.cabrera@icloud.com>
2026-05-31 05:39:53 -0700

    Create TIMELINE.m

---
+++ TIMELINE.m
@@ -0,0 +1,9 @@
+# Timeline
+
+Case 1 - The Falling Man
+Case 2 - Harbor Death
+Case 3 - Hotel Mystery
+Case 4 - Missing Witness
+Case 5 - Hale Connection
+Case 6 - The King's Reveal
+Finale - Victor Hale Exposed
